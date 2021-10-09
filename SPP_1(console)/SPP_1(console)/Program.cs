00c38198f7f49d8db440395e31b973e9e2558346
pk <p.k@gmail.com>
2021-10-09 14:13:09 +0300

    console application added

---
+++ SPP_1(console)/SPP_1(console)/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using MyLib_1;
+namespace SPP_1_console_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите путь к файлу:");
+            String path = Console.ReadLine();
+            Sort.MergeSort(path);
+            Console.WriteLine("Файл отсортирован");
+        }
+    }
+}
